0f8685f11791d38ea07ca5ec08921a8c52b42b76
renzhiwei <729079155@qq.com>
2019-11-26 17:45:28 +0800

    nuxt首次提交

---
+++ pages/news/index.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div>
+      <h2>News Index page</h2>
+      <p>NewsID:{{$route.params.newsId}}</p>
+       <ul>
+        <li><a href="/">Home</a></li>
+        <!-- <li><a href="/news/123">News-1</a></li> -->
+        <li><nuxt-link :to="{name:'news-id',params:{id:123}}">News-1</nuxt-link></li>
+        <li><nuxt-link :to="{name:'news-id',params:{id:456}}">News-2</nuxt-link></li>
+
+        <!-- <li><a href="/news/456">News-2</a></li> -->
+      </ul>
+  </div>
+</template>
